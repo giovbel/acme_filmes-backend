4d98aafba07f3588b42fb7745e1cb7193c7a5b69
GIOVANNA SILVA BELO <23111786@CFP127ALUNO.LOCAL>
2024-04-11 17:22:36 -0300

    fazendo crud do genero

---
+++ model/DAO/genero.js
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ * Objetivo: Arquivo responsável pela manipulação de dados no Banco de Dados MySQL,
+ * aqui realizamos o CRUD (Create, Read, Update, Delete) ultilizando a linguagem SQL
+ * Data: 11/04/2024
+ * Autora: Giovanna
+ * Versão: 1.0
+ * *********************************************************************************/
+
+//import da biblioteca do prisma client
+const { PrismaClient } = require('@prisma/client');
+
+//instância da classe prisma client
+const prisma = new PrismaClient();
+
+//função para inserir novo gênero no banco de dados
+const insertFilme = async function() {
+
+  let sql;
+}   
